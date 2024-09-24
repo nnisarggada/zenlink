bea1a9dd1a53d9c6eda36ed196869fa7d9ab4dd5
nnisarggada <gadannisarg@gmail.com>
2024-09-24 15:19:41 +0530

    Updated readme

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
-# Zen Link - URL Shortener
+# Zen Link - Simple URL Shortener
 
-Zen Link is a simple and user-friendly web-based URL shortening service that allows you to shorten long URLs quickly and easily. With Zen Link, you can create custom shortened links for your URLs, making them easier to share and manage.
+Zen Link is a lightweight, web-based URL shortening service that allows you to shorten long URLs quickly and easily. With Zen Link, you can create custom shortened links, making it easier to share and manage your URLs.
+
+# Table of Contents
+
+- [Zen Link - Simple URL Shortener](#zen-link---simple-url-shortener)
+  - [Features](#features)
+  - [Getting Started](#getting-started)
+    - [Prerequisites](#prerequisites)
+    - [Installation](#installation)
+    - [Configuration](#configuration)
+    - [Running the App](#running-the-app)
+  - [Usage](#usage)
+    - [Shorten a URL](#shorten-a-url)
+    - [Share a Shortened URL](#share-a-shortened-url)
+    - [Delete Links](#delete-links)
+    - [Command-Line Usage (curl/wget)](#command-line-usage-curlwget)
+  - [TODO](#todo)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Code of Conduct](#code-of-conduct)
 
 ## Features
 
 - Shorten long URLs to create compact and easy-to-share links.
 - Customize shortened links with user-defined names (e.g., `short.nnisarg.in/my-short-url`).
-- Easily copy and share shortened links with others.
+- Automatic deletion of shortened links after a specified time.
+- Command-line support for URL shortening via curl or wget.
 
 ## Getting Started
 
@@ -42,10 +62,14 @@ pip install -r requirements.txt
 
 ### Configuration
 
-Edit the Zen Link configuration in the `main.py` file to customize settings such as the hosted URL and the time until links are deleted. Modify the following variables as needed:
+Edit the Zen Link configuration in the `.env` or `main.py` file to customize settings such as the hosted URL, port, and expiration time for shortened links. Modify the following variables as needed:
 
 ```python
-URL = "localhost"  # Url of the hosted app
+# -------------------------------------------------------------------
+# The following configuration variables should be updated before running the app
+# -------------------------------------------------------------------
+
+URL = "localhost"  # URL of the hosted app
 PORT = 5000  # Port on which the app will run (Not for prod)
 MINUTES_TO_EXPIRE = 24 * 60  # Number of minutes before a short URL expires (Default is one day)
 ```
@@ -58,7 +82,7 @@ Run the Zen Link app:
 gunicorn -b 0.0.0.0:5000 main:app
 ```
 
-Here, `5000` is the port on which the app will run. You can access the Zen Link web interface in your web browser at http://localhost:5000.
+Here, `5000` is the port on which the app will run. You can access the Zen Link web interface in your web browser at [http://localhost:5000](http://localhost:5000).
 
 ## Usage
 
@@ -83,10 +107,16 @@ Shortened links are automatically deleted after the configured time.
 Zen Link supports URL shortening using command-line tools like curl or wget. For example:
 
 ```bash
-curl -F "url=example.com" -F "short_url=my-short-url" http://localhost:80/shorten
+curl -F "url=example.com" -F "short_url=my-short-url" http://localhost:5000/shorten
 ```
 
-This will shorten the URL `example.com` to http://localhost:80/my-short-url, which will get deleted after the configured time.
+This will shorten the URL `example.com` to [http://localhost:5000/my-short-url](http://localhost:5000/my-short-url), which will get deleted after the configured time.
+
+## TODO
+
+- [ ] Add rate limiting.
+- [ ] Add logging for URL creation and access.
+- [ ] Develop an admin dashboard for managing shortened URLs and monitoring usage.
 
 ## License
 
--- main.py
+++ main.py
@@ -1,42 +1,50 @@
-from flask import Flask, render_template, request, redirect, jsonify, abort
+from datetime import datetime, timedelta
+import json
+import os
 import random
 import string
-import json
 import threading
 import time
-import os
-from datetime import datetime, timedelta
+
+from flask import Flask, redirect, render_template, request
 
 # -------------------------------------------------------------------
 # The following variables need to be changed before running the app:
 # -------------------------------------------------------------------
 
-URL = "localhost" # Url of the hosted app
-PORT = 5000 # Port of the hosted app (Not for prod)
-MINUTES_TO_EXPIRE = 24 * 60 # Number of minutes before a short URL expires (Default is one day)
+URL = "localhost"  # Url of the hosted app
+PORT = 5000  # Port of the hosted app (Not for prod)
+MINUTES_TO_EXPIRE = (
+    24 * 60
+)  # Number of minutes before a short URL expires (Default is one day)
 
 # -------------------------------------------------------------------
 
 app = Flask(__name__)
 
+
 def load_url_database_from_file():
-    if os.path.exists('url_database.json'):
-        if os.path.getsize('url_database.json') == 0:
-            with open('url_database.json', 'w') as json_file:
-                json_file.write('{}')
-        with open('url_database.json', 'r') as json_file:
+    if os.path.exists("url_database.json"):
+        if os.path.getsize("url_database.json") == 0:
+            with open("url_database.json", "w") as json_file:
+                json_file.write("{}")
+        with open("url_database.json", "r") as json_file:
             return json.load(json_file)
     return {}
 
+
 url_database = load_url_database_from_file()
 
+
 def save_url_database_to_file():
-    with open('url_database.json', 'w') as json_file:
+    with open("url_database.json", "w") as json_file:
         json.dump(url_database, json_file)
+
 
 def generate_short_url():
     characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(characters) for _ in range(6))
+    return "".join(random.choice(characters) for _ in range(6))
+
 
 def delete_expired_short_urls():
     while True:
@@ -44,7 +52,9 @@ def delete_expired_short_urls():
         current_time = datetime.now()
         to_delete = []
         for short_url, data in url_database.items():
-            expiration_time = datetime.strptime(data['expiration_time'], '%Y-%m-%d %H:%M:%S')
+            expiration_time = datetime.strptime(
+                data["expiration_time"], "%Y-%m-%d %H:%M:%S"
+            )
             if current_time >= expiration_time:
                 to_delete.append(short_url)
 
@@ -55,61 +65,65 @@ def delete_expired_short_urls():
 
         time.sleep(10)
 
+
 delete_thread = threading.Thread(target=delete_expired_short_urls)
 delete_thread.daemon = True
 delete_thread.start()
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', host_url=URL)
+    return render_template("index.html", host_url=URL)
 
-@app.route('/', methods=['POST'])
+
+@app.route("/", methods=["POST"])
 def shorten():
-    original_url = request.form['url']
+    original_url = request.form["url"]
 
-    short_url = request.form.get('short_url', generate_short_url())
+    short_url = request.form.get("short_url", generate_short_url())
 
     if short_url == "":
         short_url = generate_short_url()
 
     if not original_url:
-        return ('Please provide a URL\n'), 400
+        return ("Please provide a URL\n"), 400
 
     if short_url in url_database or short_url.lower() == "about":
-        return ('Short URL already exists. Please choose a different one.\n'), 400
+        return ("Short URL already exists. Please choose a different one.\n"), 400
 
     creation_time = datetime.now()
     expiration_time = creation_time + timedelta(minutes=MINUTES_TO_EXPIRE)
     url_database[short_url.lower()] = {
-        'original_url': original_url,
-        'expiration_time': expiration_time.strftime('%Y-%m-%d %H:%M:%S')
+        "original_url": original_url,
+        "expiration_time": expiration_time.strftime("%Y-%m-%d %H:%M:%S"),
     }
 
     save_url_database_to_file()
 
-    user_agent = request.headers.get('User-Agent')
-    if 'curl' in user_agent.lower() or 'wget' in user_agent.lower():
+    user_agent = request.headers.get("User-Agent")
+    if "curl" in user_agent.lower() or "wget" in user_agent.lower():
         return "https://" + URL + "/" + short_url + "\n"
     else:
-        return render_template('success.html', url=URL, link=short_url)
+        return render_template("success.html", url=URL, link=short_url)
 
 
-@app.route('/<short_url>', methods=['GET'])
+@app.route("/<short_url>", methods=["GET"])
 def redirect_to_original(short_url):
     if short_url.lower() in url_database:
         mapping = url_database[short_url.lower()]
-        original_url = mapping['original_url']
-        if original_url.startswith('http'):
+        original_url = mapping["original_url"]
+        if original_url.startswith("http"):
             return redirect(original_url)
         else:
-            return redirect('http://' + original_url)
+            return redirect("http://" + original_url)
     else:
-        return ('Invalid Link\n'), 404
+        return ("Invalid Link\n"), 404
 
-@app.route('/about', methods=['GET'])
+
+@app.route("/about", methods=["GET"])
 def about():
-    return render_template('about.html', full_url=URL)
+    return render_template("about.html", full_url=URL)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=PORT, debug=True)
